6bf6c487793aa7dc44b11650bca0ea7d5a8ff1cd
热爱可抵万难 <2426912619@qq.com>
2026-05-04 23:28:45 +0800

    文件改用alioss

--- 后端/title/src/main/java/web/config/WebConfigConfiguration.java
+++ 后端/title/src/main/java/web/config/WebConfigConfiguration.java
@@ -21,7 +21,7 @@ public class WebConfigConfiguration implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册登录拦截器，排除注册和登录接口
        registry.addInterceptor(loginInterceptor)
-               .addPathPatterns("/**")
+               .addPathPatterns("/user/**")
                .excludePathPatterns("/user/register", "/user/login", "/user/logout","/common/**");
     }
     @Override
